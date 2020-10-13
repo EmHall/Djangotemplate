e9570d3ec86c676ed0effc5c1ecef27b9d70c11d
EmHall <em.cm.hall@gmail.com>
2020-10-13 10:31:09 +0000

    False again

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
